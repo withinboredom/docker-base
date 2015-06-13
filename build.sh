5ff8548cf3830d9f25ea28efb8286997f3090e09
Rob Landers <landers.robert@gmail.com>
2015-06-13 03:48:18 -0400

    updated builds/puhses

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-docker build -t withinboredom/ubuntu-base ubuntu-base
+docker build -t withinboredom/ubuntu-base:latest ubuntu-base
+docker tag -f ubuntu:14.04 withinboredom/ubuntu-base:base
 #docker push withinboredom/ubuntu-base
